8a5e44133d6d31225884f6f08140f7c9a5cc4a81
VEDANT KOKANE <98166789+vedantkokane10@users.noreply.github.com>
2023-10-15 22:06:58 +0530

    Create 4_  Max Number of K-Sum Pairs.cpp

---
+++ Two Pointers/4_  Max Number of K-Sum Pairs.cpp
@@ -0,0 +1,43 @@
+/*
+1679. Max Number of K-Sum Pairs
+
+You are given an integer array nums and an integer k.
+In one operation, you can pick two numbers from the array whose sum equals k and remove them from the array.
+Return the maximum number of operations you can perform on the array.
+
+Example 1:
+Input: nums = [1,2,3,4], k = 5
+Output: 2
+Explanation: Starting with nums = [1,2,3,4]:
+- Remove numbers 1 and 4, then nums = [2,3]
+- Remove numbers 2 and 3, then nums = []
+There are no more pairs that sum up to 5, hence a total of 2 operations.
+
+Example 2:
+Input: nums = [3,1,3,4,3], k = 6
+Output: 1
+Explanation: Starting with nums = [3,1,3,4,3]:
+- Remove the first two 3's, then nums = [1,4,3]
+There are no more pairs that sum up to 6, hence a total of 1 operation.
+*/
+
+int maxOperations(vector<int>& nums, int k) {
+    sort(nums.begin(),nums.end());
+    int s = 0 , e = nums.size()-1;
+    int ans = 0;
+    while(s < e){
+        int target = nums[s] + nums[e];
+        if(target == k){
+            ans++;
+            s++;
+            e--;
+        }
+        else if(target < k){
+            s++;
+        }
+        else{
+            e--;
+        }
+    }
+    return ans;
+}
